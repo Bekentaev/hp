73a4d18ec8f9a8ee4b705d2aa6058374b892432e
iva <114473393+Bekentaev@users.noreply.github.com>
2023-11-20 20:07:51 +0600

    fix fix ifx

--- main.js
+++ main.js
@@ -2,14 +2,13 @@ const audio = document.getElementById("audio");
 const playB = document.getElementById("play");
 const pauseB = document.getElementById("pause");
 
-
-
+document.addEventListener('DOMContentLoaded', function () {
+    audio.src = "1.mp3";
+    audio.play();
+});
 
 const playAudio = () => {
-    audio.src = "1.mp3";
-
     audio.play();
-
     playB.classList.toggle('disabled');
     pauseB.classList.remove('disabled');
 }
@@ -18,8 +17,36 @@ const pauseAudio = () => {
     audio.pause();
     pauseB.classList.toggle('disabled');
     playB.classList.remove('disabled');
-
 }
 
-playB.addEventListener('click', playAudio)
-pauseB.addEventListener('click', pauseAudio)
+playB.addEventListener('click', playAudio);
+pauseB.addEventListener('click', pauseAudio);
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollDuration = 20000; // Время в миллисекундах (10 секунд)
+    const targetScroll = document.body.scrollHeight - window.innerHeight;
+
+    // Анимированная прокрутка
+    function scrollToBottom() {
+        let startTime;
+
+        function animateScroll(timestamp) {
+            if (!startTime) startTime = timestamp;
+
+            const progress = timestamp - startTime;
+            const percentage = Math.min(progress / scrollDuration, 1);
+
+            window.scrollTo(0, percentage * targetScroll);
+
+            if (progress < scrollDuration) {
+                requestAnimationFrame(animateScroll);
+            }
+        }
+
+        requestAnimationFrame(animateScroll);
+    }
+
+    // Вызываем анимацию после короткой задержки (например, 1 секунды)
+    setTimeout(scrollToBottom, 1000);
+});
